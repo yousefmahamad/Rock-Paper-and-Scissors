ad3dab331d1996b018d5d2240e02a17eb4a1f1fc
yousef mohamad abdala <136375522+yousefmahamad@users.noreply.github.com>
2023-07-07 00:26:21 -0700

    Update main.py
    change some thing
    not

--- main.py
+++ main.py
@@ -1,51 +1,35 @@
 import random
 
+
+def rock_paper_scissors():
+    choices = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(choices)
+    user_choice = input('Enter your choice (rock/paper/scissors): ')
+    print(f'You chose {user_choice}.')
+    print(f'The computer chose {computer_choice}.')
+    if user_choice == computer_choice:
+        print('It\'s a tie!')
+    elif user_choice == 'rock':
+        if computer_choice == 'paper':
+            print('You lose!')
+        else:
+            print('You win!')
+    elif user_choice == 'paper':
+        if computer_choice == 'scissors':
+            print('You lose!')
+        else:
+            print('You win!')
+    elif user_choice == 'scissors':
+        if computer_choice == 'rock':
+            print('You lose!')
+        else:
+            print('You win!')
+    else:
+        print('Invalid input. Please try again.')
+
+while True:
+    rock_paper_scissors()
+    play_again = input('Do you want to play again? (yes/no): ')
+    if play_again.lower() != 'yes':
+        break
 #Main DEV : Yousef Mohamad
-
-def game_win(comp, you):
-    if comp == you:
-        return None
-
-    elif comp == 'r':
-        if you == 'p':
-            return True
-        elif you == 's':
-            return False
-
-    elif comp == 'p':
-        if you == 's':
-            return True
-        elif you == 'r':
-            return False
-
-    elif comp == 's':
-        if you == 'r':
-            return True
-        elif you == 'p':
-            return False
-
-
-print("Comp's Turn: Rock(r) Paper(p) or Scissors(s)?")
-Comp_Input = random.randint(1, 3)
-if Comp_Input == 1:
-    comp = 'r'
-elif Comp_Input == 2:
-    comp = 'p'
-elif Comp_Input == 3:
-    comp = 's'
-
-name = input("Enter your Name: ")
-you = input(f"{name}'s Turn: Rock(r) Paper(p) or Scissors(s)?")
-x = game_win(comp, you)
-
-print(f"Computer chose : {comp}")
-print(f"{name} chose : {you}")
-
-if x == None:
-    print("The Game is a Tie!!")
-elif x:
-    print(f"Congratulations!!! {name} Won The Game")
-else:
-    print(f"{name} Lost The Game!!!")
-
-print("Thankyou!!! for playing our game")
